ee8e6b053624269cc28aa520d138993174c853c2
Deyvison J Paula <deyvisonjp@id.uff.br>
2020-08-06 15:28:35 -0300

    Finalizando Components e estilizações

--- aulas/web/src/pages/TeacherForm/index.js
+++ aulas/web/src/pages/TeacherForm/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 
 import PageHeader from '../../components/PageHeader';
 import Input from '../../components/Input';
+import TextArea from '../../components/TextArea';
+
 
 import warningIcon from '../../assets/images/icons/warning.svg'
 import './style.css';
+import Select from '../../components/Select';
 
 function TeacherForm() {
   return (
@@ -22,12 +24,49 @@ function TeacherForm() {
           <Input name="name" label="Nome Completo" />
           <Input name="avatar" label="Avatar" />
           <Input name="whatsapp" label="whatsapp" />
+          <TextArea name="bio" label="Biografia" />
         </fieldset>
 
         <fieldset>
           <legend>Sobre a aula</legend>
-          <Input name="subject" label="Matéria" />
+          <Select
+            name="subject"
+            label="Matéria"
+            options={[
+              { value: 'Artes', label: 'Artes' },
+              { value: 'Biologia', label: 'Biologia' },
+              { value: 'Matemática', label: 'Matemática' },
+              { value: 'Português', label: 'Português' },
+              { value: 'Computação', label: 'Computação' }
+            ]}
+          />
           <Input name="cost" label="Custo da sua hora por aula" />
+        </fieldset>
+
+        <fieldset>
+          <legend>
+            Horários disponíveis
+              <button type="button">* Novo horário</button>
+          </legend>
+
+          <div className="schedule-item">
+
+            <Select
+              name="week_day"
+              label="Dia da semana"
+              options={[
+                { value: '0', label: 'Domingo' },
+                { value: '1', label: 'Segunda-feira' },
+                { value: '2', label: 'Terça-feira' },
+                { value: '3', label: 'Quarta-feira' },
+                { value: '4', label: 'Quinta-feira' },
+                { value: '5', label: 'Sexta-feira' },
+                { value: '6', label: 'Sabádo' }
+              ]}
+            />
+            <Input name="from" label="Das" type="time" />
+            <Input name="to" label="Até" type="time" />
+          </div>
         </fieldset>
 
         <footer>
--- aulas/web/src/pages/TeacherList/index.js
+++ aulas/web/src/pages/TeacherList/index.js
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom';
 import PageHeader from '../../components/PageHeader';
 import TeacherItem from '../../components/TeacherItem';
 import Input from '../../components/Input';
+import Select from '../../components/Select';
 
 import './style.css';
 
@@ -13,16 +14,38 @@ function TeacherList() {
       <PageHeader title="Esses são os proffys disponíveis">
         <form id="search-teachers">
 
-          <Input name="subject" label="Matéria" />
-          <Input name="week_day" label="Dia da semana" />
+          <Select
+            name="subject"
+            label="Matéria"
+            options={[
+              { value: 'Artes', label: 'Artes' },
+              { value: 'Biologia', label: 'Biologia' },
+              { value: 'Matemática', label: 'Matemática' },
+              { value: 'Português', label: 'Português' },
+              { value: 'Computação', label: 'Computação' }
+            ]}
+          />
+
+          <Select
+            name="week_day"
+            label="Dia da semana"
+            options={[
+              { value: '0', label: 'Domingo' },
+              { value: '1', label: 'Segunda-feira' },
+              { value: '2', label: 'Terça-feira' },
+              { value: '3', label: 'Quarta-feira' },
+              { value: '4', label: 'Quinta-feira' },
+              { value: '5', label: 'Sexta-feira' },
+              { value: '6', label: 'Sabádo' }
+            ]}
+          />
+
           <Input type="time" name="time" label="Hora" />
 
         </form>
       </PageHeader>
 
       <main>
-        <TeacherItem />
-        <TeacherItem />
         <TeacherItem />
         <TeacherItem />
       </main>
